5f1ea5ee6906e96401f33958b3e0b68eadd4fbc4
PatrickLumenus <patrickaluy@gmail.com>
2022-12-08 15:53:14 -0500

    Defined Tests for CharacterSet

--- packages/util/cypress/e2e/character-set.cy.ts
+++ packages/util/cypress/e2e/character-set.cy.ts
@@ -1,5 +1,27 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { CharacterSet } from './../../lib/core';
+
+describe('Generating CharacterSet instances', () => {
+  it('generate a UTF8 instance from value', () => {
+    expect(new CharacterSet('UTF-8')).to.be.an('object')
+  });
+
+  it('should generate a UTF-8 instance from the factory function.', () => {
+    expect(CharacterSet.UTF8()).to.be.an('object');
+  });
+
+  it('generate a ASCII instance from value', () => {
+    expect(new CharacterSet('ASCII')).to.be.an('object')
+  });
+
+  it('should generate a ASCII instance from the factory function.', () => {
+    expect(CharacterSet.ASCII()).to.be.an('object');
+  });
+
+  it('generate a ANSI instance from value', () => {
+    expect(new CharacterSet('ANSI')).to.be.an('object')
+  });
+
+  it('generate a 8859 instance from value', () => {
+    expect(new CharacterSet('8859')).to.be.an('object')
+  });
+});
